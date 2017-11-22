c4ceae200aebdc75d2d4baa2fc72a6df70eae1ad
Benjamin Kleiner <bizzl@users.sourceforge.net>
2017-11-22 18:23:16 +0100

    Added furaffinity bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,7 @@
 <?php
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
@@ -27,6 +27,7 @@ class AppKernel extends Kernel
             new DownloadApp\App\DownloadBundle\DownloadBundle(),
             new DownloadApp\Scanners\CoreBundle\CoreBundle(),
             new DownloadApp\Scanners\DeviantArtBundle\DeviantArtBundle(),
+            new DownloadApp\Scanners\FurAffinityBundle\FurAffinityBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/Scanners/FurAffinityBundle/Command/SetCookiesCommand.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * Copyright (c) 2017 Benjamin Kleiner
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace DownloadApp\Scanners\FurAffinityBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class SetCookiesCommand
+ * @package DownloadApp\Scanners\FurAffinityBundle\Command
+ */
+class SetCookiesCommand extends ContainerAwareCommand
+{
+    const NAME = 'furaffinity:cookies:import';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Import furaffinity cookies for a user')
+            ->addArgument('user', InputArgument::REQUIRED)
+            ->addArgument('json', InputArgument::REQUIRED, 'JSON file with exported cookies');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $currentUserService = $this
+            ->getContainer()
+            ->get('downloadapp.user.current');
+        $user = $this
+            ->getContainer()
+            ->get('fos_user.user_provider.username')
+            ->loadUserByUsername($input->getArgument('user'));
+        $currentUserService->setUser($user);
+        $cookieJar = $this
+            ->getContainer()
+            ->get('downloadapp.user.cookiejars.furaffinity');
+        $cookieUtilService = $this
+            ->getContainer()
+            ->get('downloadapp.utils.cookies');
+
+        $cookies = json_decode(file_get_contents($input->getArgument('json')), true);
+
+        $cookieUtilService->importCookies($cookies, $cookieJar);
+    }
+}
